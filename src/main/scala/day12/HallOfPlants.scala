86eaed11fbb9937634ae553b858e4f842eb87f67
kelley.glenn <kelley.glenn@workday.com>
2020-01-11 00:10:56 -0800

    Day 12, part 2

--- src/main/scala/day12/HallOfPlants.scala
+++ src/main/scala/day12/HallOfPlants.scala
@@ -1,30 +1,74 @@
 package day12
 
-class HallOfPlants(var plants: Seq[Boolean], val rules: Map[(Boolean, Boolean, Boolean, Boolean, Boolean), Boolean]) {
-  var idxOfCentralPot: Int = 0
+import java.time.LocalDateTime
 
-  def runGenerations(gens: Int): Unit = {
-    val buffer: Seq[Boolean] = (1 to (gens + 2)).map(_ < 0)
-    plants = buffer ++ plants ++ buffer
-    idxOfCentralPot += gens + 2
-    val staticPlantIdxs = Set(0, 1, plants.size - 2, plants.size - 1)
-    (1 to gens).foreach { _: Int =>
-      plants = for (x <- plants.indices) yield {
-        if (staticPlantIdxs.contains(x)) plants(x)
-        else rules.getOrElse((plants(x - 2), plants(x - 1), plants(x), plants(x + 1), plants(x + 2)), false)
+class HallOfPlants(val plants: Seq[Boolean], val rules: Map[(Boolean, Boolean, Boolean, Boolean, Boolean), Boolean]) {
+
+  var (firstPot, lastPot: Pot) = Pot.fromSeq(plants, 0)
+  var centerPot: Pot = firstPot
+
+  private def generateBufferSeq(size: Int): Seq[Boolean] = {
+    (1 to size).map((_: Int) => false)
+  }
+
+  private def addBuffer(size: Int): Unit = {
+    val newPre: (Pot, Pot) = Pot.fromSeq(generateBufferSeq(size), firstPot.number - size)
+    val newPost: (Pot, Pot) = Pot.fromSeq(generateBufferSeq(size), lastPot.number + 1)
+    newPre._2.right = firstPot
+    firstPot.left = newPre._2
+    firstPot = newPre._1
+    lastPot.right = newPost._1
+    newPost._1.left = lastPot
+    lastPot = newPost._2
+  }
+
+  private def trim(): Unit = {
+    while (!firstPot.value) firstPot = firstPot.right
+    firstPot.left = null
+    while (!lastPot.value) lastPot = lastPot.left
+    lastPot.right = null
+  }
+
+  def runGenerations(gens: Long): Unit = {
+    addBuffer(4)
+    var curGen: Long = 0
+    var previousGen: Pot = firstPot
+    while (curGen < gens) {
+      var pm2: Pot = firstPot
+      var pm1: Pot = pm2.right
+      var p: Pot = pm1.right
+      var pp1: Pot = p.right
+      var pp2: Pot = pp1.right
+      firstPot = Pot(pm2.value, pm2.number)
+      lastPot = Pot(pm1.value, pm1.number)
+      firstPot.right = lastPot
+      lastPot.left = firstPot
+      while (pp2 != null) {
+        lastPot =
+          Pot(rules.getOrElse((pm2.value, pm1.value, p.value, pp1.value, pp2.value), false), p.number, lastPot, null)
+        lastPot.left.right = lastPot
+        pm2 = pm1; pm1 = p; p = pp1; pp1 = pp2; pp2 = pp2.right
+      }
+      lastPot = Pot(p.value, p.number, lastPot, null)
+      lastPot.left.right = lastPot
+      lastPot = Pot(pp1.value, pp1.number, lastPot, null)
+      lastPot.left.right = lastPot
+      addBuffer(2)
+      curGen += 1
+      if (curGen % 5L == 0L) {
+        trim()
+        addBuffer(4)
       }
     }
-    idxOfCentralPot -= plants.indexWhere(_ == true)
-    plants = plants.dropWhile(_ == false)
-    plants = plants.take(plants.lastIndexWhere(_ == true) + 1)
+    trim()
   }
 
   def mkString: String = {
-    plants.map(if (_) '#' else '.').mkString
+    Pot.toSeq(firstPot).map((p: (Boolean, Int)) => if (p._1) '#' else '.').mkString
   }
 
   def numbersOfPotsWithPlants: Seq[Int] =
-    plants.zipWithIndex.filter(_._1 == true).map((p: (Boolean, Int)) => p._2 - idxOfCentralPot)
+    Pot.toSeq(firstPot).filter(_._1 == true).map((p: (Boolean, Int)) => p._2)
 }
 
 object HallOfPlants {
@@ -44,5 +88,32 @@ object HallOfPlants {
   def stringsToPlantsAndRules(
       strings: Seq[String]): (Seq[Boolean], Map[(Boolean, Boolean, Boolean, Boolean, Boolean), Boolean]) = {
     (stringToPlants(strings.head.split(": ")(1)), stringsToRules(strings.drop(2)))
+  }
+}
+
+case class Pot(value: Boolean, number: Int, var left: Pot = null, var right: Pot = null) {
+
+  override def equals(obj: Any): Boolean = {
+    obj match {
+      case other: Pot => value == other.value && number == other.number && right == other.right
+      case _          => false
+    }
+  }
+}
+
+object Pot {
+
+  def fromSeq(seq: Seq[Boolean], firstNumber: Integer): (Pot, Pot) = {
+    val nodes: Seq[Pot] = seq.zipWithIndex.map((p: (Boolean, Int)) => Pot(p._1, p._2 + firstNumber))
+    (0 until nodes.size - 1).foreach { i: Int =>
+      nodes(i).right = nodes(i + 1)
+      nodes(i + 1).left = nodes(i)
+    }
+    (nodes.head, nodes.last)
+  }
+
+  def toSeq(p: Pot): Seq[(Boolean, Int)] = {
+    if (p == null) Seq.empty
+    else (p.value, p.number) +: toSeq(p.right)
   }
 }
--- src/test/scala/day12/HallOfPlantsTest.scala
+++ src/test/scala/day12/HallOfPlantsTest.scala
@@ -45,6 +45,30 @@ class HallOfPlantsTest extends AnyFlatSpec {
     hall.runGenerations(20)
     assert(hall.numbersOfPotsWithPlants.sum == 2045)
   }
+  it should "solve the second puzzle" in new SetupPuzzleData("input") {
+
+    val params: (Seq[Boolean], Map[(Boolean, Boolean, Boolean, Boolean, Boolean), Boolean]) =
+      HallOfPlants.stringsToPlantsAndRules(lines)
+    var hall = new HallOfPlants(params._1, params._2)
+    hall.runGenerations(500)
+    assert(hall.numbersOfPotsWithPlants.sum == 21428)
+    hall = new HallOfPlants(params._1, params._2)
+    hall.runGenerations(5000)
+    assert(hall.numbersOfPotsWithPlants.sum == 210428)
+    hall = new HallOfPlants(params._1, params._2)
+    hall.runGenerations(10000)
+    assert(hall.numbersOfPotsWithPlants.sum == 420428)
+    hall = new HallOfPlants(params._1, params._2)
+    hall.runGenerations(20000)
+    assert(hall.numbersOfPotsWithPlants.sum == 840428)
+    hall = new HallOfPlants(params._1, params._2)
+    hall.runGenerations(30000)
+    assert(hall.numbersOfPotsWithPlants.sum == 1260428)
+    hall = new HallOfPlants(params._1, params._2)
+    hall.runGenerations(30000)
+    assert(hall.numbersOfPotsWithPlants.sum == (30000 / 500 * 21000) + 428)
+    assert(((50000000000L / 500L * 21000L) + 428L) == 2100000000428L)
+  }
 
   class SetupExampleData {
 
